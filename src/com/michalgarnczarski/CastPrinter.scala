a2f23726fa1040896e9d006141a92e04e2023b32
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-26 17:53:08 +0100

    printSingleDice function aded

--- src/com/michalgarnczarski/CastPrinter.scala
+++ src/com/michalgarnczarski/CastPrinter.scala
@@ -3,8 +3,17 @@ package com.michalgarnczarski
 object CastPrinter {
 
   def print(castResult: List[Int]): String = {
-    val line1: String = "\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 "
-    val line5: String = "\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 "
-    line1 + line5
+
+    def printSingleDice(result: Int): String = {
+      val line1: String = "\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 "
+      val line2: String = ""
+      val line3: String = ""
+      val line4: String = ""
+      val line5: String = "\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 "
+      line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + line5
+    }
+
+
+    printSingleDice(0)
   }
 }
