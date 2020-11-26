1ca22b809b74cf4e7506e287e2cee61de979d7d3
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-26 18:06:30 +0100

    printSingleDice implemented

--- src/com/michalgarnczarski/CastPrinter.scala
+++ src/com/michalgarnczarski/CastPrinter.scala
@@ -6,14 +6,36 @@ object CastPrinter {
 
     def printSingleDice(result: Int): String = {
       val line1: String = "\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 "
-      val line2: String = ""
-      val line3: String = ""
-      val line4: String = ""
       val line5: String = "\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 "
-      line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + line5
+
+      val empty: String = "\u2502       \u2502 "
+      val oneLeft: String = "\u2502 O     \u2502 "
+      val oneMiddle: String = "\u2502   O   \u2502 "
+      val oneRight: String = "\u2502     O \u2502 "
+      val two: String = "\u2502 O   O \u2502 "
+
+      val line2: String = result match {
+        case 1 => empty
+        case 2 | 3 => oneLeft
+        case 4 | 5 | 6 => two
+      }
+
+      val line3: String = result match {
+        case 1 | 3 | 5 => oneMiddle
+        case 2 | 4 => empty
+        case 6 => two
+      }
+
+      val line4: String = result match {
+        case 1 => empty
+        case 2 | 3 => oneRight
+        case 4 | 5 | 6 => two
+      }
+
+      line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5
     }
 
 
-    printSingleDice(0)
+    printSingleDice(castResult(0))
   }
 }
