1bd90b6b150a40641d5602eecb92cd681811927a
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-26 17:47:53 +0100

    Print function initiated

--- src/com/michalgarnczarski/CastPrinter.scala
+++ src/com/michalgarnczarski/CastPrinter.scala
@@ -2,4 +2,9 @@ package com.michalgarnczarski
 
 object CastPrinter {
 
+  def print(castResult: List[Int]): String = {
+    val line1: String = "\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 "
+    val line5: String = "\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 "
+    line1 + line5
+  }
 }
