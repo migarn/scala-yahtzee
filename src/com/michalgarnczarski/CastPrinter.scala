5c91e9409166856e907bdd2492b95cf7469f5e93
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-28 13:08:57 +0100

    printSingleDice refactored

--- src/com/michalgarnczarski/CastPrinter.scala
+++ src/com/michalgarnczarski/CastPrinter.scala
@@ -5,31 +5,29 @@ object CastPrinter {
   def print(castResult: List[Int]): String = {
 
     def printSingleDice(result: Int): String = {
+
+      val emptyLine: String = "\u2502       \u2502 "
+      val lineWithLeftOne: String = "\u2502 O     \u2502 "
+      val lineWithMiddleOne: String = "\u2502   O   \u2502 "
+      val lineWithRightOne: String = "\u2502     O \u2502 "
+      val lineWithTwo: String = "\u2502 O   O \u2502 "
+
       val line1: String = "\u250c\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 "
       val line5: String = "\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 "
-
-      val empty: String = "\u2502       \u2502 "
-      val oneLeft: String = "\u2502 O     \u2502 "
-      val oneMiddle: String = "\u2502   O   \u2502 "
-      val oneRight: String = "\u2502     O \u2502 "
-      val two: String = "\u2502 O   O \u2502 "
-
       val line2: String = result match {
-        case 1 => empty
-        case 2 | 3 => oneLeft
-        case 4 | 5 | 6 => two
+        case 1 => emptyLine
+        case 2 | 3 => lineWithLeftOne
+        case 4 | 5 | 6 => lineWithTwo
       }
-
       val line3: String = result match {
-        case 1 | 3 | 5 => oneMiddle
-        case 2 | 4 => empty
-        case 6 => two
+        case 1 | 3 | 5 => lineWithMiddleOne
+        case 2 | 4 => emptyLine
+        case 6 => lineWithTwo
       }
-
       val line4: String = result match {
-        case 1 => empty
-        case 2 | 3 => oneRight
-        case 4 | 5 | 6 => two
+        case 1 => emptyLine
+        case 2 | 3 => lineWithRightOne
+        case 4 | 5 | 6 => lineWithTwo
       }
 
       line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5
