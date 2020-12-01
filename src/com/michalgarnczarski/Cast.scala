8e166c5b8ca8d7eab77b8de410a5529c17868dad
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-01 18:10:32 +0100

    randomDiceValue and reviseCast functions implemented

--- src/com/michalgarnczarski/Cast.scala
+++ src/com/michalgarnczarski/Cast.scala
@@ -4,5 +4,9 @@ import scala.util.Random
 
 object Cast {
 
-  def generateCast(diceQuantity: Int): List[Int] = List.fill(diceQuantity)(Random.nextInt(6) + 1)
+  private def randomDiceValue: Int = Random.nextInt(6) + 1
+
+  def generateCast(diceQuantity: Int): List[Int] = List.fill(diceQuantity)(randomDiceValue)
+
+  def reviseCast(cast: List[Int]): List[Int] = cast.map(dice => if (dice == 0) randomDiceValue else dice)
 }
