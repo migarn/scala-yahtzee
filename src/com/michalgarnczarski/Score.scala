70697c3835b9c37a5c5d47b48169d6ba7d7541b9
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-02 17:10:32 +0100

    print function implemented

--- src/com/michalgarnczarski/Score.scala
+++ src/com/michalgarnczarski/Score.scala
@@ -5,4 +5,6 @@ class Score {
   val scores: List[Int] = List.fill(8)(-1)
   val labels: List[String] = List("Aces", "Twos", "Threes", "Fours", "Fives", "Sixes", "Bonus", "Score")
 
+  def print: String = scores.zip(labels).map {case (x, y) => y + ": " + x + " pts."}.mkString("\n")
+
 }
