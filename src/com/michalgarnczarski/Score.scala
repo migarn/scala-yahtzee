3430153b498df681c82292dab28937101fa4e81a
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-02 16:56:38 +0100

    labels implemented

--- src/com/michalgarnczarski/Score.scala
+++ src/com/michalgarnczarski/Score.scala
@@ -3,5 +3,6 @@ package com.michalgarnczarski
 class Score {
 
   val scores: List[Int] = List.fill(8)(-1)
+  val labels: List[String] = List("Aces", "Twos", "Threes", "Fours", "Fives", "Sixes", "Bonus", "Score")
 
 }
