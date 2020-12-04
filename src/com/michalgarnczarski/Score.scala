87501e69b0b7ef9715ad83a38288ce8867a52749
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-04 17:18:02 +0100

    addScore implemented

--- src/com/michalgarnczarski/Score.scala
+++ src/com/michalgarnczarski/Score.scala
@@ -3,8 +3,12 @@ package com.michalgarnczarski
 class Score {
 
   val scores: List[Int] = List.fill(8)(-1)
+
   val labels: List[String] = List("Aces", "Twos", "Threes", "Fours", "Fives", "Sixes", "Bonus", "Score")
 
   def print: String = scores.zip(labels).map {case (x, y) => f"$y%-8s $x%3d pts."}.mkString("\n")
+
+  def addScore(cast: List[Int], chosenFigures: Int): List[Int] =
+    scores.updated(chosenFigures - 1, cast.count(_ == chosenFigures) * chosenFigures)
 
 }
