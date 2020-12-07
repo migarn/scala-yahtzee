5b9b14602551634a26ce47792a9fea4a59d2ce7c
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-07 18:27:12 +0100

    scores type changed to MutableList

--- src/com/michalgarnczarski/Score.scala
+++ src/com/michalgarnczarski/Score.scala
@@ -1,21 +1,25 @@
 package com.michalgarnczarski
 
+import scala.collection.mutable.MutableList
+
 class Score {
 
-  val scores: List[Int] = List.fill(8)(-1)
+  val scores: MutableList[Int] = MutableList.fill(8)(-1)
 
   val labels: List[String] = List("Aces", "Twos", "Threes", "Fours", "Fives", "Sixes", "Bonus", "Score")
 
   def print: String = scores.zip(labels).map {case (x, y) => f"$y%-8s $x%3d pts."}.mkString("\n")
 
-  def addScore(cast: List[Int], chosenFigures: Int): List[Int] =
-    scores.updated(chosenFigures - 1, cast.count(_ == chosenFigures) * chosenFigures)
-
-  def calculateScore(): List[Int] = {
-    val upperSectionSum: Int = scores.filter(_ != -1).sum
-    val bonus: Int = if (upperSectionSum >= 63) 35 else 0
-    scores.updated(6, bonus)
-    scores.updated(7, upperSectionSum + bonus)
+  def addScore(cast: List[Int], chosenFigures: Int): MutableList[Int] = {
+    scores.update(chosenFigures - 1, cast.count(_ == chosenFigures) * chosenFigures)
+    scores
   }
 
+  def calculateScore(): MutableList[Int] = {
+    val upperSectionSum: Int = scores.filter(_ != -1).sum
+    val bonus: Int = if (upperSectionSum >= 63) 35 else 0
+    scores.update(6, bonus)
+    scores.update(7, upperSectionSum + bonus)
+    scores
+  }
 }
