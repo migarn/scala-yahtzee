e672b0137147363fa586571d7f0367421ae1b87d
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-08 18:19:42 +0100

    Player implemented

--- src/com/michalgarnczarski/Player.scala
+++ src/com/michalgarnczarski/Player.scala
@@ -1,5 +1,6 @@
 package com.michalgarnczarski
 
-class Player {
-
+class Player(val name: String, val playingMethod: PlayingMethod) {
+  val score: Score = new Score
+  def takeTurn(): Score = playingMethod.takeTurn(score)
 }
