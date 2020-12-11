185526314fd522693fb38d24073e155256925cda
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-11 18:23:48 +0100

    defineAvailableFigures implemented

--- src/com/michalgarnczarski/ManualPlaying.scala
+++ src/com/michalgarnczarski/ManualPlaying.scala
@@ -4,6 +4,16 @@ import com.michalgarnczarski.InputScanner._
 
 class ManualPlaying extends PlayingMethod {
 
+  def calculateScore(score: Score, cast: List[Int]): Score = {
+
+  }
+
+  def defineAvailableFigures(score: Score): List[Int] = {
+    for {i <- List.range(0, 5)
+         if score.scores(i) == -1
+    } yield i + 1
+  }
+
   def sleep(): Unit = Thread.sleep(1000)
 
 }
