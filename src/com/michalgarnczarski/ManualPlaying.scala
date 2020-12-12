912668326c00ddbadabc4ed59edfd881242f030a
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-12-12 13:15:19 +0100

    calculateScore implemented

--- src/com/michalgarnczarski/ManualPlaying.scala
+++ src/com/michalgarnczarski/ManualPlaying.scala
@@ -4,14 +4,21 @@ import com.michalgarnczarski.InputScanner._
 
 class ManualPlaying extends PlayingMethod {
 
-  def calculateScore(score: Score, cast: List[Int]): Score = {
-
-  }
+  def printAvailableFigures(score: Score): String = {""}
 
   def defineAvailableFigures(score: Score): Array[Int] = {
     for {i <- Array.range(0, 5)
          if score.scores(i) == -1
     } yield i + 1
+  }
+
+  def calculateScore(score: Score, cast: List[Int]): String = {
+    val chosenFigure: Int = scanIntForSelectionList(
+      "\nSelect figures to be scored:" + printAvailableFigures(score), defineAvailableFigures(score): _*)
+    score.addScore(cast, chosenFigure)
+    println("\nYour score:")
+    score.calculateScore()
+    score.print
   }
 
   def sleep(): Unit = Thread.sleep(1000)
